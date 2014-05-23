97f0ebdc7bfe3915cb41bf59dea46a7a8a49ee9e
Koji Tanaka <kj.tanaka@gmail.com>
2014-05-23 14:42:12 -0400

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,11 +7,25 @@
 # All rights reserved - Do Not Redistribute
 #
 
-secrets = Chef::EncryptedDataBagItem.load("slurm", "secrets")
+#secrets = Chef::EncryptedDataBagItem.load("slurm", "secrets")
 
-munge_key = secrets['munge_key']
+slurm_control_machine = node['slurm']['control_machine']
+slurm_procs = node['slurm']['procs']
+slurm_node_name = node['slurm']['node_name']
+slurm_state_save_location = node['slurm']['state_save_location']
+slurm_log_dir = node['slurm']['log_dir']
 
-packages = %w{munge}
+# install necessary packages
+packages = %w[
+	rpm-build
+	readline
+	readline-devel
+	openssl-devel
+	munge-devel
+	pam-devel
+	perl-ExtUtils-MakeMaker
+	perl-DBI
+]
 
 packages.each do |pkg|
 	package pkg do
@@ -19,12 +33,57 @@ packages.each do |pkg|
 	end
 end
 
-script "create_munge_key" do
-	interpreter "bash"
+slurm_download_url = "http://www.schedmd.com/download/latest/slurm-14.03.3-2.tar.bz2"
+
+remote_file "/tmp/slurm-14.03.3-2.tar.bz2" do
+	source slurm_download_url
+	mode "0644"
+	checksum "760320e81511198a7b723a00c9b96948"
+end
+
+execute "rpmbuild_slurm" do
 	user "root"
-	code <<-EOH
-		echo #{munge_key} > /etc/munge/munge.key
-		chmod 0400 /etc/munge/munge.key
-	EOH
-	creates "/etc/munge/munge.key"
+	cwd "/tmp"
+	command "rpmbuild -ta slurm-14.03.3-2.tar.bz2"
+	creates "/root/rpmbuild/RPMS/x86_64/"
+end
+
+execute "rpminstall_slurm" do
+	user "root"
+	command "rpm --install /root/rpmbuild/RPMS/x86_64/*.rpm"
+	creates "/etc/slurm/"
+end
+
+directory slurm_state_save_location do
+	owner "root"
+	group "root"
+	mode "0755"
+	recursive true
+	action :create
+end
+
+directory slurm_log_dir do
+	owner "root"
+	group "root"
+	mode "0755"
+	recursive true
+	action :create
+end
+
+template "/etc/slurm/slurm.conf" do
+	owner "root"
+	group "root"
+	variables(
+		:slurm_control_machine => slurm_control_machine,
+		:slurm_procs => slurm_procs,
+		:slurm_node_name => slurm_node_name,
+		:slurm_state_save_location => slurm_state_save_location,
+		:slurm_log_dir => slurm_log_dir
+	)
+	notifies :restart, "service[slurm]", :immediately
+end
+
+service "slurm" do
+	supports :restart => true
+	action [:enable, :start]
 end
