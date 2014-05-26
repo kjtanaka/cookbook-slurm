79cb09d6525ccc04b7c42c5a84cd4e8805bae886
Koji Tanaka <kj.tanaka@gmail.com>
2014-05-26 16:22:28 -0400

    update recipe::default and metadata.rb

--- Berksfile
+++ Berksfile
@@ -1,2 +1,8 @@
 source 'https://api.berkshelf.com'
+
+cookbook 'yum-epel'
+cookbook 'build-essential'
+cookbook 'selinux'
+cookbook 'munge', git: 'https://github.com/kjtanaka/cookbook-munge.git'
+
 metadata
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,25 @@
 #
 # Cookbook Name:: slurm
 # Recipe:: default
+# Author:: Koji Tanaka (<kj.tanaka@gmail.com>)
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, FutureGrid, Indiana Univercity
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-#secrets = Chef::EncryptedDataBagItem.load("slurm", "secrets")
+include_recipe "munge"
+include_recipe "build-essential"
+include_recipe "selinux::disabled"
 
 slurm_control_machine = node['slurm']['control_machine']
 slurm_procs = node['slurm']['procs']
